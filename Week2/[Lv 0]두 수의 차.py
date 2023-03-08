879c589f0e62044098c52b8de36701e094a240c8
강승훈 <102217780+SeunghoonSeunghoon@users.noreply.github.com>
2023-03-08 23:12:10 +0900

    Delete Week2 directory

--- Week2/[Lv 0]두 수의 차.py
+++
@@ -1,4 +0,0 @@
-def solution(num1, num2): # 쉬운거 풀고싶었어
-    
-    answer = num1 - num2
-    return answer
--- Week2/[Lv 0]배열 뒤집기.py
+++
@@ -1,3 +0,0 @@
-def solution(num_list):
-    answer = num_list.reverse()
-    return num_list
